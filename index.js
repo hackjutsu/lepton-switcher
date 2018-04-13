31c354187277d4154e766a41582b230a2eabffd3
hackjutsu <cosmo.lepton@gmail.com>
2018-04-12 22:02:47 -0700

    enhance: adopt stylish consola

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const fs = require('fs')
 const home = require('os').homedir()
+const consola = require('consola')
 
 const leptonrcFile = home + '/.leptonrc'
-let version = 'GitHub'
-if (fs.existsSync(leptonrcFile)) {
-  const content = fs.readFileSync(leptonrcFile)
-  const config = JSON.parse(content)
-  if (config.enterprise) {
-    if (config.enterprise.enable) {
-      console.log('----> Switching to Lepton for GitHub...')
-      config.enterprise.enable = false
-    } else {
-      console.log('----> Switching to Lepton for GitHub Enterprise...')
-      config.enterprise.enable = true
-      version += ' Enterprise'
-    }
-  } else {
-    console.log('----> Please enable Lepton for GitHub Enterprise manually for the first time.')
-  }
-  fs.writeFileSync(leptonrcFile, JSON.stringify(config, null, 2), 'utf8')
-  console.log(`----> Congrats!! Lepton for ${version} is enabled. Restart Lepton to see the changes.`)
+if (!fs.existsSync(leptonrcFile)) {
+  consola.warn('   ~/.leptonrc not found.')
+  process.exit(-1)
 }
+
+let version = 'GitHub'
+const content = fs.readFileSync(leptonrcFile)
+const config = JSON.parse(content)
+
+if (!config.enterprise) {
+  consola.warn('   Please enable Lepton for GitHub Enterprise manually for the first time.')
+  process.exit(-1)
+}
+
+if (config.enterprise.enable) {
+  consola.start('  Switching Lepton to GitHub...')
+  config.enterprise.enable = false
+} else {
+  consola.start('  Switching Lepton to GitHub Enterprise...')
+  config.enterprise.enable = true
+  version += ' Enterprise'
+}
+
+fs.writeFileSync(leptonrcFile, JSON.stringify(config, null, 2), 'utf8')
+consola.success(`Congrats! Lepton for ${version} is enabled. Restart Lepton to see the changes.`)
